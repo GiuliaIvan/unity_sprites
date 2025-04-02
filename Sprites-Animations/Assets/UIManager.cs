75329afe3f535fd111296ef6663e3c7ce3504c91
charliebrownisdead <bejanm93@gmail.com>
2025-04-02 13:27:37 +0200

    code

---
+++ Sprites-Animations/Assets/UIManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void LoadScene (int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
