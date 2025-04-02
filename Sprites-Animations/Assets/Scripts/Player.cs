4185be0e6c84dde671de3764fdb75ef4c8e6bb13
charliebrownisdead <bejanm93@gmail.com>
2025-04-02 12:17:21 +0200

    shark

--- Sprites-Animations/Assets/Scripts/Player.cs
+++ Sprites-Animations/Assets/Scripts/Player.cs
@@ -3,49 +3,54 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private Animator animator;
 
     [Header("-- Private references --")]
     [SerializeField] private int coinsCollected = 0;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        animator = GetComponent<Animator>(); // Get the Animator component
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 newPosition = transform.position;
-            if(Input.GetKey(KeyCode.A)) {
-                newPosition.x -= moveSpeed * Time.deltaTime;
-            }
-            if(Input.GetKey(KeyCode.D)) {
-                newPosition.x += moveSpeed * Time.deltaTime;
-            }
-            if(Input.GetKey(KeyCode.W)) {
-                newPosition.y += moveSpeed * Time.deltaTime;
-            }
-            if(Input.GetKey(KeyCode.S)) {
-                newPosition.y -= moveSpeed * Time.deltaTime;
-            }            
+        bool isMoving = false; // Track movement to update animation
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            newPosition.x -= moveSpeed * Time.deltaTime;
+            animator.SetInteger("Walk", 1); // Set walking animation
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            newPosition.x += moveSpeed * Time.deltaTime;
+            animator.SetInteger("Walk", 2); // Set walking animation
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            newPosition.y += moveSpeed * Time.deltaTime;
+            animator.SetInteger("Walk", 3); // Set walking animation
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            newPosition.y -= moveSpeed * Time.deltaTime;
+            animator.SetInteger("Walk", 4); // Set walking animation
+            isMoving = true;
+        }
+
+        if (!isMoving) {
+            animator.SetInteger("Walk", 0); // Stop animation when idle
+        }
+
         transform.position = newPosition;
     }
 
-    // OnTriggerEnter2D is called once, when two GameObjects with Collider2Ds hit each other
-        // - One GameObject must have a Rigidbody2D as well
-        // - One of the Collider2Ds must be a Trigger
-    void OnTriggerEnter2D(Collider2D coll) {
-        if(coll.gameObject.tag.Equals("Coin")) {
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Coin")) {
             coinsCollected += 1;
             Destroy(coll.gameObject);
         }
     }
-
 }
